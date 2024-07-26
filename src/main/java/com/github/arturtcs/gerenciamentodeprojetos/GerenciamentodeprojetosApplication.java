5de95ead217807e3bfc062ce5aa27aa64f4cf0a1
arturtarcisio <arturtarcisio1@gmail.com>
2024-07-26 14:59:22 -0300

    Projeto criado e suas dependencias iniciais

---
+++ src/main/java/com/github/arturtcs/gerenciamentodeprojetos/GerenciamentodeprojetosApplication.java
@@ -0,0 +1,13 @@
+package com.github.arturtcs.gerenciamentodeprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodeprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodeprojetosApplication.class, args);
+	}
+
+}
