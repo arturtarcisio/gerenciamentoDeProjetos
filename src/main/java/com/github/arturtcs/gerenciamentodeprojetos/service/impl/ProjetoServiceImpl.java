0080373bc3ef4743fece78a3172caffcc4d81245
arturtarcisio <arturtarcisio1@gmail.com>
2024-07-27 19:17:50 -0300

    Refatoracao da implementacao do service de projetos

--- src/main/java/com/github/arturtcs/gerenciamentodeprojetos/service/impl/ProjetoServiceImpl.java
+++ src/main/java/com/github/arturtcs/gerenciamentodeprojetos/service/impl/ProjetoServiceImpl.java
@@ -6,7 +6,6 @@ import com.github.arturtcs.gerenciamentodeprojetos.exceptions.ResourceNotFoundEx
 import com.github.arturtcs.gerenciamentodeprojetos.model.Cliente;
 import com.github.arturtcs.gerenciamentodeprojetos.model.Projeto;
 import com.github.arturtcs.gerenciamentodeprojetos.model.dto.ProjetoDTO;
-import com.github.arturtcs.gerenciamentodeprojetos.repositories.AtividadeRepository;
 import com.github.arturtcs.gerenciamentodeprojetos.repositories.ClienteRepository;
 import com.github.arturtcs.gerenciamentodeprojetos.repositories.ProjetoRepository;
 import com.github.arturtcs.gerenciamentodeprojetos.service.ProjetoService;
@@ -33,110 +32,77 @@ public class ProjetoServiceImpl implements ProjetoService {
 
     @Override
     public ProjetoDTO cadastrarProjeto(ProjetoDTO projetoDTO) {
+        validarProjetoDTO(projetoDTO);
 
-        ValidacoesUtil.validarNome(projetoDTO.nome());
-
-        // Verifica se o nome do projeto já existe
-        if (projetoRepository.existsByNome(projetoDTO.nome())) {
-            throw new ConflitoAtributoException("Nome do projeto já existente");
-        }
-
-        ValidacoesUtil.validaStatusDoProjeto(projetoDTO.status());
-
-        // Verifica se o cliente foi informado
-        if (projetoDTO.clienteId() == null) {
-            throw new RegraDeNegocioException("Cliente deve ser informado para cadastro do projeto");
-        }
-
-        // Verifica se o cliente existe
         Cliente cliente = clienteRepository.findById(projetoDTO.clienteId())
-                .orElseThrow(() -> new ResourceNotFoundException("O cliente ao qual você deseja vincular ao projeto não existe na base de dados"));
+                .orElseThrow(() -> new ResourceNotFoundException("O cliente não existe na base de dados"));
 
-        // Cria o novo projeto
         Projeto novoProjeto = Projeto.builder()
                 .nome(projetoDTO.nome())
                 .status(projetoDTO.status())
                 .cliente(cliente)
                 .build();
 
-        // Salva o projeto no repositório
-        Projeto projetoSalvo = projetoRepository.save(novoProjeto);
-
-        // Retorna o DTO do projeto salvo
-        return new ProjetoDTO(
-                projetoSalvo.getId(),
-                projetoSalvo.getNome(),
-                projetoSalvo.getStatus(),
-                projetoSalvo.getCliente().getId()
-        );
+        return converterParaDTO(projetoRepository.save(novoProjeto));
     }
 
     @Override
     public ProjetoDTO atualizarProjeto(Long id, ProjetoDTO projetoDTO) {
-
         Projeto projetoExistente = projetoRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Projeto não encontrado"));
 
-        ValidacoesUtil.validarNome(projetoDTO.nome());
+        validarProjetoDTO(projetoDTO);
 
         if (!projetoExistente.getNome().equals(projetoDTO.nome()) && projetoRepository.existsByNome(projetoDTO.nome())) {
-            throw new RegraDeNegocioException("Nome do projeto já existente");
+            throw new ConflitoAtributoException("Nome do projeto já existente");
         }
 
-        if (projetoDTO.clienteId() != null) {
-            Cliente cliente = clienteRepository.findById(projetoDTO.clienteId())
-                    .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado. Favor informar um cliente que está cadastrado na base de dados."));
-            projetoExistente.setCliente(cliente);
-        } else {
-            throw new RegraDeNegocioException("Cliente deve ser informado.");
-        }
+        Cliente cliente = clienteRepository.findById(projetoDTO.clienteId())
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
 
         projetoExistente.setNome(projetoDTO.nome());
-
-        ValidacoesUtil.validaStatusDoProjeto(projetoDTO.status());
         projetoExistente.setStatus(projetoDTO.status());
+        projetoExistente.setCliente(cliente);
 
-        Projeto projetoAtualizado = projetoRepository.save(projetoExistente);
-
-        return new ProjetoDTO(
-                projetoAtualizado.getId(),
-                projetoAtualizado.getNome(),
-                projetoAtualizado.getStatus(),
-                projetoAtualizado.getCliente().getId()
-        );
+        return converterParaDTO(projetoRepository.save(projetoExistente));
     }
 
     @Override
     public void deletarProjeto(Long id) {
-        projetoRepository.findById(id)
-                .map(projeto -> {
-                    projetoRepository.delete(projeto);
-                    return Void.TYPE;
-                })
-                .orElseThrow(() -> new ResourceNotFoundException("Projeto não existe na base de dados para ser deletado."));
+        Projeto projeto = projetoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Projeto não encontrado"));
+        projetoRepository.delete(projeto);
     }
 
     @Override
     public List<ProjetoDTO> listarProjetos() {
         return projetoRepository.findAll().stream()
-                .map(projeto -> new ProjetoDTO(
-                        projeto.getId(),
-                        projeto.getNome(),
-                        projeto.getStatus(),
-                        projeto.getCliente().getId()
-                ))
+                .map(this::converterParaDTO)
                 .collect(Collectors.toList());
     }
 
     @Override
     public ProjetoDTO listarProjetoPorId(Long id) {
         return projetoRepository.findById(id)
-                .map(projeto -> new ProjetoDTO(
-                        projeto.getId(),
-                        projeto.getNome(),
-                        projeto.getStatus(),
-                        projeto.getCliente().getId()
-                ))
+                .map(this::converterParaDTO)
                 .orElseThrow(() -> new ResourceNotFoundException("Projeto não encontrado"));
+    }
+
+    private void validarProjetoDTO(ProjetoDTO projetoDTO) {
+        ValidacoesUtil.validarNome(projetoDTO.nome());
+        ValidacoesUtil.validaStatusDoProjeto(projetoDTO.status());
+
+        if (projetoDTO.clienteId() == null) {
+            throw new RegraDeNegocioException("Cliente deve ser informado");
+        }
+    }
+
+    private ProjetoDTO converterParaDTO(Projeto projeto) {
+        return new ProjetoDTO(
+                projeto.getId(),
+                projeto.getNome(),
+                projeto.getStatus(),
+                projeto.getCliente().getId()
+        );
     }
 }
