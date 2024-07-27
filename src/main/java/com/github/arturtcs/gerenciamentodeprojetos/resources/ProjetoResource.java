5a5bc289be5f4a3af3cf4c99aab358da941159f8
arturtarcisio <arturtarcisio1@gmail.com>
2024-07-27 17:41:04 -0300

    Criado recurso endpoints para projetos

--- src/main/java/com/github/arturtcs/gerenciamentodeprojetos/resources/ProjetoResource.java
+++ src/main/java/com/github/arturtcs/gerenciamentodeprojetos/resources/ProjetoResource.java
@@ -5,13 +5,11 @@ import com.github.arturtcs.gerenciamentodeprojetos.model.dto.ProjetoDTO;
 import com.github.arturtcs.gerenciamentodeprojetos.service.ProjetoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/projetos")
@@ -29,6 +27,28 @@ public class ProjetoResource {
                 .buildAndExpand(projetoDTO.id())
                 .toUri();
         return ResponseEntity.created(uri).body(projetoDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjetoDTO>> listarTodosOsProjetos() {
+        return ResponseEntity.ok(projetoService.listarProjetos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjetoDTO> listarProjetoPorId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(projetoService.listarProjetoPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProjeto(@PathVariable Long id) {
+        projetoService.deletarProjeto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjetoDTO> atualizarProjeto(@PathVariable Long id, @RequestBody ProjetoDTO projetoAtualizado) {
+        var clienteAtualizado = projetoService.atualizarProjeto(id, projetoAtualizado);
+        return ResponseEntity.ok().body(clienteAtualizado);
     }
 
 }
